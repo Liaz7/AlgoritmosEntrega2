2cb46a5bb6d91f6b7ed131997470c943bc15ad0f
Lucas <lucasnacho42@gmail.com>
2023-11-06 20:15:21 -0300

    Update entidades y colas

---
+++ SistemaAutogestion/SistemaAutogestion/src/tads/ICola.java
@@ -0,0 +1,17 @@
+
+package colas;
+
+/**
+ *
+ * @author usuario
+ */
+public interface ICola <T extends Comparable<T>>{
+    public void encolar(NodoCola nodo);
+    public void desencolar();
+    public boolean esVacia();
+    public boolean esLlena();
+    public int elementos();
+    public NodoCola frente();
+    public void mostrarCola();
+    public void mostrarREC(NodoCola nodo);
+}
---
+++ SistemaAutogestion/SistemaAutogestion/src/tads/NodoCola.java
@@ -0,0 +1,32 @@
+
+package colas;
+
+
+public class NodoCola <T extends Comparable<T>>{
+
+
+    private T dato;
+    private NodoCola<T> sig;
+
+    public NodoCola(T dato) {
+        this.dato = dato;
+        this.sig = null;
+    }
+
+
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public NodoCola<T> getSig() {
+        return sig;
+    }
+
+    public void setSig(NodoCola<T> sig) {
+        this.sig = sig;
+    }    
+}
